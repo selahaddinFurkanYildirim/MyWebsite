af65d5897c90074c48a893185f42edf09403bb93
selahaddinFurkanYildirim <110099952+selahaddinFurkanYildirim@users.noreply.github.com>
2023-09-14 10:41:53 +0300

    Add files via upload

---
+++ www/data/userid\.php
@@ -0,0 +1,19 @@
+<?php
+if($_SERVER['HTTP_USER_AGENT'] !== false) {
+    echo "you can't do this";
+    http_response_code(403); //FORBIDDEN
+    exit;
+}
+?>
+root
+
+iğdi
+iğdi
+iğdi
+yıldırım
+
+yıldırım
+yıldırım
+yıldırım
+kerimcan
+yr
---
+++ www/data/username\.php
@@ -0,0 +1,19 @@
+<?php
+if($_SERVER['HTTP_USER_AGENT'] !== false) {
+    echo "you can't do this";
+    http_response_code(403); //FORBIDDEN
+    exit;
+}
+?>
+root
+root
+meliha
+zehra
+ahmet
+furkan
+furkan
+ali
+avşar
+semiha
+kerimcan
+yusufAbi
